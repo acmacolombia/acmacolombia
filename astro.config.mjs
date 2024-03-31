56387e0f8d1f92ac39509aa065e88e8d54e0012e
Maria Alejandra Botero <marialeja199@gmail.com>
2024-03-31 18:57:58 -0500

    Add i18n and sitemap configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import { defineConfig, squooshImageService } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
@@ -13,7 +14,7 @@ import tasks from './src/utils/tasks';
 
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
 
-import { ANALYTICS, SITE } from './src/utils/config.ts';
+import { ANALYTICS, SITE, I18N } from './src/utils/config.ts';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -28,9 +29,13 @@ export default defineConfig({
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
-    fallback: {
-      en: "es"
-    },
+    // locales: ["es", {
+    //   path: "en", // no slashes included
+    //   codes: ["en"]
+    // }],
+    // fallback: {
+    //   en: "es"
+    // },
     routing: {
         prefixDefaultLocale: false
     }
@@ -45,6 +50,25 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    // Conditionally add i18n and sitemap based on I18N.isEnabled
+    ...(I18N.isEnabled
+      ? [
+          // i18n({
+          //   locales: I18N.locales,
+          //   defaultLocale: I18N.defaultLocale,
+          // }),
+          sitemap({
+            i18n: {
+              locales: I18N.locales,
+              defaultLocale: I18N.defaultLocale,
+            },
+            filter: filterSitemapByDefaultLocale({ defaultLocale: I18N.defaultLocale }),
+          }),
+        ]
+      : [
+          sitemap({}),
+        ]),
+
     sitemap(),
     mdx(),
     icon({
